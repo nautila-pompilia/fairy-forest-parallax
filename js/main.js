b685f47f88d7263391dbaa13c4df00702507ccea
DariaRaiRai <dari.shamrai@gmail.com>
2024-07-26 14:36:47 -0400

    parallax forest
    Smooth scroll is added
    scroll libs are uploaded and registered

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 window.addEventListener("scroll", (e) => {
-  document.body.style.cssText = `--scrollTop: ${this.scrollY}px`; // concotination
+  document.body.style.cssText += `--scrollTop: ${this.scrollY}px`; // concotination
+});
+
+gsap.registerePlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+  wrapper: ".wrapper",
+  content: ".content",
 });
